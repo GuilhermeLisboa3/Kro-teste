1524e12f2ccce8243f926e8dad66ef8b248e563e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-01 21:31:53 -0300

    test: ensume GetDataCsv call CpfValidator with correct value

--- tests/domain/use-cases/read-csv-file.spec.ts
+++ tests/domain/use-cases/read-csv-file.spec.ts
@@ -1,16 +1,23 @@
-import { type ReadCsvFile } from '@/domain/contracts/gateways'
+import { contractParams } from '@/tests/mocks'
+import { type ReadCsvFile, type CpfValidator } from '@/domain/contracts/gateways'
 import { type GetDataCsv, getDataCsvUseCase } from '@/domain/use-cases'
 
 import { mock } from 'jest-mock-extended'
 
 describe('GetDataCsv', () => {
   let sut: GetDataCsv
+  const contract = contractParams
   const error = new Error('error')
 
   const fileCsv = mock<ReadCsvFile>()
+  const validator = mock<CpfValidator>()
+
+  beforeAll(() => {
+    fileCsv.readFile.mockResolvedValue([contract])
+  })
 
   beforeEach(() => {
-    sut = getDataCsvUseCase(fileCsv)
+    sut = getDataCsvUseCase(fileCsv, validator)
   })
 
   it('should call ReadCsvFile', async () => {
@@ -26,5 +33,12 @@ describe('GetDataCsv', () => {
     const promise = sut()
 
     await expect(promise).rejects.toThrow(error)
+  })
+
+  it('should call CpfValidator with correct value', async () => {
+    await sut()
+
+    expect(validator.validate).toHaveBeenCalledWith({ cpf: contract.nrCpfCnpj })
+    expect(validator.validate).toHaveBeenCalledTimes(1)
   })
 })
