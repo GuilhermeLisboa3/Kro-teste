82041e64ddc92014fb4128f01a20d60bfef9b0f7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-04 08:57:10 -0300

    feat: ensure ValidatorAdapter call cpnj.isValid with correct value

--- src/infra/gateways/validator-adapter.ts
+++ src/infra/gateways/validator-adapter.ts
@@ -1,9 +1,13 @@
-import { type CpfValidator } from '@/domain/contracts/gateways'
+import { type CpfValidator, type CnpjValidator } from '@/domain/contracts/gateways'
 
-import { cpf } from 'cpf-cnpj-validator'
+import { cpf, cnpj } from 'cpf-cnpj-validator'
 
 export class ValidatorAdapter implements CpfValidator {
   async cpfValidator (input: CpfValidator.Input): Promise<CpfValidator.Output> {
     return cpf.isValid(input.cpf.toString())
+  }
+
+  async cnpjValidator (input: CnpjValidator.Input): Promise<any> {
+    cnpj.isValid(input.cnpj.toString())
   }
 }
