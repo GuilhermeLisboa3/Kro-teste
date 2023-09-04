e009e97f25cc1054e7adbb0b8577360b535757d9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-04 11:11:27 -0300

    feat: ensure ConvertReal return data formart pt-br

--- src/infra/gateways/convert-adapter.ts
+++ src/infra/gateways/convert-adapter.ts
@@ -1,6 +1,14 @@
-import { type ConvertReal } from '@/domain/contracts/gateways'
+import { type ConvertReal, type ConvertDate } from '@/domain/contracts/gateways'
 export class ConvertAdapter implements ConvertReal {
   async real ({ value }: ConvertReal.Input): Promise<ConvertReal.Output> {
     return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
+  }
+
+  async date ({ value }: ConvertDate.Input): Promise<ConvertDate.Output> {
+    const year = value.toString().slice(0, 4)
+    const month = value.toString().slice(4, 6)
+    const day = value.toString().slice(6, 8)
+    const date = new Date(`${year}-${month}-${day}`)
+    return date.toLocaleDateString('pt-br', { timeZone: 'UTC' })
   }
 }
