43042a2fc6640eb665390af656d0b3077d6fcd6e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-04 11:51:17 -0300

    chore: add express and config

---
+++ src/main/config/middleware.ts
@@ -0,0 +1,9 @@
+import { cors, bodyParser, contentType } from '@/main/middlewares'
+
+import { type Express } from 'express'
+
+export const setupMiddlewares = (app: Express): void => {
+  app.use(cors)
+  app.use(bodyParser)
+  app.use(contentType)
+}
---
+++ src/main/server.ts
@@ -0,0 +1,4 @@
+import '@/main/config/module-alias'
+import { app } from '@/main/config/app'
+
+app.listen(3000, () => { 'Server running at http://localhost:3000' })
