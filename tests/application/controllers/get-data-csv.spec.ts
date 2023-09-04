746f9510aa3bf5c98acde60a3418121a7f465d5b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-04 11:37:10 -0300

    test: ensure GetDataCsvController return serverError if getDataCsv return error

--- tests/application/controllers/get-data-csv.spec.ts
+++ tests/application/controllers/get-data-csv.spec.ts
@@ -1,4 +1,5 @@
 import { GetDataCsvController } from '@/application/controllers'
+import { ServerError } from '@/application/errors'
 
 describe('GetDataCsvController', () => {
   let sut: GetDataCsvController
@@ -13,5 +14,13 @@ describe('GetDataCsvController', () => {
 
     expect(getDataCsv).toHaveBeenCalled()
     expect(getDataCsv).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return serverError if getDataCsv return error', async () => {
+    getDataCsv.mockRejectedValueOnce(new Error())
+    const { statusCode, data } = await sut.handle()
+
+    expect(statusCode).toBe(500)
+    expect(data).toEqual(new ServerError())
   })
 })
