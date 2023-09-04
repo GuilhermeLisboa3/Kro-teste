2b6f30951be2595faa4b65fa7b5fc5c494b918c1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-04 07:58:22 -0300

    feat: ensure ReadCsvFileAdapter call csv-parse

--- src/infra/gateways/read-csv-file-adapter.ts
+++ src/infra/gateways/read-csv-file-adapter.ts
@@ -1,7 +1,9 @@
 import { createReadStream } from 'fs'
+import csv from 'csv-parser'
 
 export class ReadCsvFileAdapter {
   async readFile (): Promise<any> {
     createReadStream('data.csv')
+      .pipe(csv())
   }
 }
