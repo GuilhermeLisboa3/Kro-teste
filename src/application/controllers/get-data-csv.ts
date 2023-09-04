8bb4af9ba728019cff0571a440f08ca6945b5fd1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-04 11:39:55 -0300

    feat: ensure GetDataCsvController return ok on success

--- src/application/controllers/get-data-csv.ts
+++ src/application/controllers/get-data-csv.ts
@@ -1,12 +1,13 @@
 import { type GetDataCsv } from '@/domain/use-cases'
-import { serverError } from '../helpers'
+import { ok, serverError } from '../helpers'
 
 export class GetDataCsvController {
   constructor (private readonly getDataCsv: GetDataCsv) { }
 
   async handle (): Promise<any> {
     try {
-      await this.getDataCsv()
+      const result = await this.getDataCsv()
+      return ok(result)
     } catch (error) {
       return serverError(error)
     }
--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -2,6 +2,11 @@ import { ServerError } from '../errors'
 
 export type HttpResponse<T = any> = { statusCode: number, data: T }
 
+export const ok = <T = any> (data: T): HttpResponse<T> => ({
+  statusCode: 200,
+  data
+})
+
 export const serverError = (error: unknown): HttpResponse<Error> => ({
   statusCode: 500,
   data: new ServerError(error instanceof Error ? error : undefined)
